4b23360594235c0add74cfa880c1c2a1f4523dec
Simon K <30438951+simshard@users.noreply.github.com>
2023-12-04 04:32:52 +0000

    get xrdata

--- app/Livewire/MYForm.php
+++ app/Livewire/MYForm.php
@@ -2,17 +2,53 @@
 
 namespace App\Livewire;
 
+use App\Models\Xcrate;
 use Livewire\Component;
 use Illuminate\Http\Request;
+use Livewire\Attributes\Validate;
 
 class MYForm extends Component
 {
+    #[Validate(['required','numeric','max_digits:2','min:1','max:12','digits_between:01,12'])] 
     public $month;
-    public $year;
+    #[Validate(['required','numeric','max_digits:2','min:22','max:24'])] 
+      public $year;
 
     public function store(Request $request)
     {
-        dd(request()->post());
+         ( $this->validate());
+       $month = $this->month ;
+       $year = $this->year;
+       $url = 'http://www.hmrc.gov.uk/softwaredevelopers/rates/exrates-monthly-'.   $month.$year.'.XML';
+       //dd($url);
+       // http://www.hmrc.gov.uk/softwaredevelopers/rates/exrates-monthly-0123.XML
+       $xml_string = file_get_contents($url);
+       $xml = simplexml_load_string($xml_string);
+       $json = json_encode($xml);
+       $data = json_decode($json, true);
+       $period = $data['@attributes']['Period'];
+
+       foreach ($data['exchangeRate'] as $index => $data) {
+           $xrdata[] =[
+               'countryName' => $data['countryName'],
+                'countryCode' => $data['countryCode'] ,
+                'currencyName'  => $data['currencyName'],
+               'currencyCode'  => $data['currencyCode'],
+                'rateNew'  => $data['rateNew'],
+               'period' => $period,
+           ];
+       }
+
+       
+        //dd($xrdata);
+       Xcrate::insert($xrdata);
+      return $this->redirect('/getxr');//->with('status', 'Exchange Rate Data stored');
+
+      //check for 404 
+      //redirect with success message
+
+
+
     }
 
     public function show( ) 
--- resources/views/livewire/m-y-form.blade.php
+++ resources/views/livewire/m-y-form.blade.php
@@ -1,13 +1,16 @@
 <div>
-    <div class="container mx-auto mt-4 px-4">
+    <div class="container mx-auto mt-16 px-8">
      
         <h1 class="mb-4">Fetch HMRC Exchange Rate</h1>
-        <form method="POST" action="/getxr" > 
+        <form 
+        wire:submit="store"
+        {{-- method="POST" action="/getxr"  --}}
+        > 
           @csrf
          
           <div class="container mx-auto mt-4 px-4">
             <label for="month">Month</label>
-            <select name="month" id="month">
+            <select name="month" id="month" wire:model="month">
               <option value="">--</option>
               <option value="01">Jan</option>
               <option value="02">Feb</option>
@@ -23,13 +26,17 @@
               <option value="12">Dec</option>
             </select>
             @error('month')
+            <span class="error">{{ $message }}</span>
             <span class="text-red-500">
               Month must be in numeric range 01>12    </span>
             @enderror
         </div>
+
+      
+
         <div>
           <label for="year">Year</label>
-          <select name="year" id="year">
+          <select name="year" id="year" wire:model="year">
             <option value="">----</option>
             <option value="22">2022</option>
             <option value="23">2023</option>
@@ -41,9 +48,12 @@
           @enderror
       </div>
      <div class="mt-6">
-             
                 <button class="button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Submit</button>
              </div>
           </form>
+         
+
+         <div class="mt-8 p-8 hover:text-red-500">
+          <a href="/dashboard">Back to Exchange rates table</a>
          </div>
-</div>
+</div></div>
